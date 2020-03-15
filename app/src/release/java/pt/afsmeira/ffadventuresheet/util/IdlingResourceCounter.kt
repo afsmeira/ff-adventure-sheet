cf2f00f74c4c4481d2a08a2f84f01dc3a5b8b6a9
André Meira <afsmeira+github@gmail.com>
2020-03-15 23:49:42 +0000

    Create `debug` and `release` build variants
    Having separate variants guarantees that the test code introduced in non-test packages is actually a no-op in the `release` variant.
    
    Closes issue #15

---
+++ app/src/release/java/pt/afsmeira/ffadventuresheet/util/IdlingResourceCounter.kt
@@ -0,0 +1,9 @@
+package pt.afsmeira.ffadventuresheet.util
+
+/**
+ * Release variant for [IdlingResourceCounter]. See the debug version for the real implementation.
+ */
+object IdlingResourceCounter {
+    fun increment() {}
+    fun decrement() {}
+}
