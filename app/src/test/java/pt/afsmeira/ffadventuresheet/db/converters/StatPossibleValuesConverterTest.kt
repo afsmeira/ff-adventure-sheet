f2c475d3d7078e75397224c5b8101b96a3d32c3d
André Meira <afsmeira+github@gmail.com>
2020-03-28 22:32:36 +0000

    Add tests for `StatValueConverter`

--- app/src/test/java/pt/afsmeira/ffadventuresheet/db/converters/StatPossibleValuesConverterTest.kt
+++ app/src/test/java/pt/afsmeira/ffadventuresheet/db/converters/StatPossibleValuesConverterTest.kt
@@ -11,35 +11,28 @@ import pt.afsmeira.ffadventuresheet.model.Stat
 @RunWith(JUnit4::class)
 class StatPossibleValuesConverterTest {
 
+    private val converter = StatPossibleValuesConverter()
+
     @Test
-    fun testConvertUndefinedPossibleValuesToString() {
-        val json = StatPossibleValuesConverter().toString(Stat.PossibleValues.Undefined)
+    fun testConvertUndefinedPossibleValues() {
+        val json = converter.toString(Stat.PossibleValues.Undefined)
+        val possibleValues = converter.fromString(json)
 
         assertThat(json, `is`("""{"type":"undefined"}"""))
+        assertThat(possibleValues, instanceOf(Stat.PossibleValues.Undefined::class.java))
     }
 
     @Test
-    fun testConvertUndefinedPossibleValuesFromString() {
-        val undefined =
-            StatPossibleValuesConverter().fromString("""{"type":"undefined"}""")
-
-        assertThat(undefined, instanceOf(Stat.PossibleValues.Undefined::class.java))
-    }
-
-    @Test
-    fun testConvertDefinedPossibleValuesToString() {
-        val possibleValues = Stat.PossibleValues.Defined(listOf("X", "Y", "Z"))
-        val json = StatPossibleValuesConverter().toString(possibleValues)
+    fun testConvertDefinedPossibleValues() {
+        val definedValues = Stat.PossibleValues.Defined(listOf("X", "Y", "Z"))
+        val json = converter.toString(definedValues)
+        val possibleValues = converter.fromString(json)
 
         assertThat(json, `is`("""{"type":"defined","values":["X","Y","Z"]}"""))
-    }
-
-    @Test
-    fun testConvertDefinedPossibleValuesFromString() {
-        val defined =
-            StatPossibleValuesConverter().fromString("""{"type":"defined","values":["X","Y","Z"]}""")
-
-        assertThat(defined, instanceOf(Stat.PossibleValues.Defined::class.java))
-        assertThat((defined as Stat.PossibleValues.Defined).values, `is`(listOf("X", "Y", "Z")))
+        assertThat(possibleValues, instanceOf(Stat.PossibleValues.Defined::class.java))
+        assertThat(
+            (possibleValues as Stat.PossibleValues.Defined).values,
+            `is`(listOf("X", "Y", "Z"))
+        )
     }
 }
