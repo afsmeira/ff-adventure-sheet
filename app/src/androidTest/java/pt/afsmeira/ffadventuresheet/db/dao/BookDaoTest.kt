b5677ed0c28cf37f1d4a1ab2314fa2a09d9dc113
André Meira <afsmeira+github@gmail.com>
2020-05-17 00:04:46 +0100

    Stop using `LiveData` for `Book` queries
    `Book` data is immutable, there is no benefit of using `LiveData` queries.

--- app/src/androidTest/java/pt/afsmeira/ffadventuresheet/db/dao/BookDaoTest.kt
+++ app/src/androidTest/java/pt/afsmeira/ffadventuresheet/db/dao/BookDaoTest.kt
@@ -1,27 +1,25 @@
 package pt.afsmeira.ffadventuresheet.db.dao
 
-import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import kotlinx.coroutines.runBlocking
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Rule
 import org.junit.Test
-import org.junit.rules.RuleChain
 import org.junit.runner.RunWith
 import pt.afsmeira.ffadventuresheet.util.FFAdventureSheetDatabaseRule
-import pt.afsmeira.ffadventuresheet.util.observeAndAwait
 
 @RunWith(AndroidJUnit4::class)
 class BookDaoTest {
 
-    var dbRule = FFAdventureSheetDatabaseRule()
-
     @Rule
     @JvmField
-    var ruleChain = RuleChain.outerRule(dbRule).around(InstantTaskExecutorRule())
+    var dbRule = FFAdventureSheetDatabaseRule()
 
     @Test
     fun listAllBooksTest() {
-        assertThat(dbRule.db.bookDao().listAll().observeAndAwait().size, `is`(69))
+        runBlocking {
+            assertThat(dbRule.db.bookDao().listAll().size, `is`(69))
+        }
     }
 }
